211e99ab5ce10316d34517f18512338b52a7b080
lacravate <david.elbaz@af83.com>
2013-10-10 12:16:18 +0200

    Version; just workin'...

--- lib/yasify/version.rb
+++ lib/yasify/version.rb
@@ -1,6 +1,11 @@
 # encoding: utf-8
 
-require 'pathstring'
+# Explaining to bundler the whole lineage is the most painless and lasting
+# way to have this working. So...
+
+class PathstringInterface < String; end
+
+class Pathstring < PathstringInterface; end
 
 class Yasify < Pathstring
   VERSION = "0.0.1"
